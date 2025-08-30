3f14c4115447aac5b63fc5ec6d31a2c47c52ccf2
lah <lah@ur.mom>
2025-08-30 19:09:25 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Hop Along Visualiser
+
+![Example gif](img/example.gif)
+
+Fun visualiser using Barry Martin Hop Along fractals (using [this document](https://www.jolinton.co.uk/Mathematics/Hopalong_Fractals/Text.pdf) as a reference).
+
+## Setup
+
+Install requirements and run main script.
+
+```sh
+python -m pip install -r requirements.txt
+python main.py
+```
+
+## Options
+
+```sh
+python main.py --help
+```
+
+### Control the number of iteration per frame
+
+```sh
+python main.py --niters 1000
+```
+
+### Control the number of frames before rendered iterations disappear
+
+```sh
+python main.py --nhist 10
+```
+
+If this value is set to 0, the frame is completely cleared with each update.
+
+### Control the number of frames before hop along parameters are reset
+
+```sh
+python main.py --nreset 10
+```
+
+If this value is set to 0, the parameters are never reset.
+Low values (e.g., 1) create very chaotic (but fun) visuals as multiple structures are layered.
+Larger values create more stable strutures, but too large and the visual can feel less dynamic.
+
+### Control target framerate
+
+```sh
+python main --fps 25
+```
+
+Defaults to 25 fps for cinematic experience although my PC can only handle about 11 fps.
+
+## Example Images
+
+![Example1](img/example1.png)
+![Example2](img/example2.png)
+![Example3](img/example3.png)
+![Example4](img/example4.png)
+![Example5](img/example5.png)
+![Example6](img/example6.png)
+![Example7](img/example7.png)
+![Example8](img/example8.png)
+![Example9](img/example7.png)
---
+++ main.py
@@ -0,0 +1,262 @@
+"""
+Hop along fun.
+
+29/08/2025.
+"""
+
+from __future__ import annotations
+
+import numba as nb
+import numpy as np
+import numpy.typing as npt
+import matplotlib.pyplot as plt
+import matplotlib
+from typing import Optional
+
+
+DEBUG = False
+
+
+@nb.njit(debug=DEBUG)
+def hop_along_eq(xy: npt.NDArray, a: float, b: float, c: float) -> npt.NDArray:
+    """
+    Complete 1 iteration of the hop along equation.
+
+    Args:
+        xy (npt.NDArray): Initial coordiates to iterate on.
+        a (float): Hop along parameter.
+        b (float): Hop along parameter.
+        c (float): Hop along parameter.
+
+    Returns:
+        npt.NDArray: The resulting coordinate from the iteration.
+    """
+    sqrt_x = np.sqrt(np.abs(b * xy[0] - c))
+    
+    f_x = -np.copysign(sqrt_x, xy[0]) # Classic BM
+    # f_x = +np.copysign(sqrt_x, xy[0]) # Positive BM, also cool
+    # f_x = sqrt_x # Additive, not bad
+    # f_x = np.sin(b*xy[0] - c) # Sinusoidal, pretty interesting
+    # f_x = np.abs(b*xy[0]) # Gingerbread Man, kinda meh
+
+    new_x = xy[1] + f_x
+    new_y = a - xy[0]
+    return np.array([new_x, new_y])
+
+
+@nb.njit(debug=DEBUG)
+def hop_along_for_n(
+    a: float,
+    b: float,
+    c: float,
+    n_iters: int,
+    xy_init: Optional[npt.NDArray] = None,
+) -> npt.NDArray:
+    """
+    Calculate `n_iters` iterations of the hop along equations for parameters (a,b,c).
+
+    Use this function to generate a sample of iterations for a set of parameters.
+    By default, this function uses (0,0) for initial conditions, but this can be ov
+
+    Args:
+        a (float):
+            Hop along parameter.
+        b (float):
+            Hop along parameter.
+        c (float):
+            Hop along parameter.
+        n_iters (int):
+            Number of iterations to calculate.
+        xy_init (float, optional):
+            Initial conditions for the iteration. Defaults to (0,0).
+
+    Returns:
+        npt.NDArray: The results of each iteration with shape (n_iters, 2).
+    """
+    ret = np.zeros((n_iters, 2))
+
+    if xy_init is None:
+        xy_init = np.array([0.0, 0.0])
+    xy = xy_init
+    for index in range(n_iters):
+        xy = hop_along_eq(xy, a, b, c)
+        ret[index, 0] = xy[0]
+        ret[index, 1] = xy[1]
+    return ret
+
+
+def random_abc(
+    min_val: float = -10.0,
+    max_val: float = +10.0,
+) -> tuple[float, float, float]:
+    """
+    Generate a random set of parameters.
+
+    Args:
+        min_val (float, optional):
+            The smallest value any of the parameters can take. Defaults to -10.0.
+        max_val (float, optional):
+            The largest value any of the parameters can take. Defaults to +10.0.
+
+    Returns:
+        tuple[float, float, float]: The generated parameters.
+    """
+    return tuple(np.random.uniform(min_val, max_val) for _ in range(3))  # type: ignore
+
+
+def plot_hop_along(
+    a: float,
+    b: float,
+    c: float,
+    n_iters: int,
+    *,
+    n_hist: int,
+    n_reset: int,
+    alpha_init: float = 0.3,
+    pause_time: float = 0.9,
+) -> None:
+    """
+    Opens a matplotlib window and continuously generates plots of hop along iterations.
+
+    Each update will calculate `n_iters` iterations of the hop along function
+    (`hop_along_eq`) for parameters (`a`, `b`, `c`) and draw them on screens.
+    Once drawn on screen, the points will remain drawn for `n_hist` future updates,
+    with their alpha decreasing linearly with each update.
+    A new update will be calcualted every 0.04s (25 fps).
+
+    There's is currently no way to make this return so good luck.
+
+    Args:
+        a (float):
+            Hop along parameter.
+        b (float):
+            Hop along parameter.
+        c (float):
+            Hop along parameter.
+        n_iters (int):
+            The number of hop along iterations to draw per frame/update.
+        n_hist (int):
+            The number of frames/updates that an interation should remain on screen.
+        n_reset (int):
+            If n_reset > 0, it is the number of frames to be calculated before
+            a new set of parameters (a,b,c) are generated.
+        alpha_init (float, optional):
+            Initial alpha value for points. Defaults to 0.3.
+        pause_time (float, optional):
+            Time to pause between frames (controls frame-rate). Defaults to 0.9.
+    """
+    # TODO figure out how to stop annoying white boarder on top and left.
+    matplotlib.use("Qt5agg")
+    plt.style.use("dark_background")
+    plt.ion()
+
+    # Store previous iterations in a list which we will use as a fifo system.
+    history = []
+
+    values = np.array([[0.0, 0.0]])
+    values = np.swapaxes(values, 0, 1)
+    full_iters = 0
+    positive_reset = n_reset > 0
+    positive_hist = n_hist > 0
+    while True:
+        # Draw next iter.
+        values = hop_along_for_n(a, b, c, n_iters, xy_init=values[:, -1])
+        values = np.swapaxes(values, 0, 1)
+        plt.scatter(
+            values[0],
+            values[1],
+            marker="*",
+            c=list(range(0, n_iters)),
+            alpha=alpha_init,
+            cmap="plasma",
+        )
+        # Draw previous iters.
+        if positive_hist:
+            for index, arr in enumerate(history):
+                alpha = (index + 1) / n_hist * alpha_init
+                plt.scatter(
+                    arr[0],
+                    arr[1],
+                    marker="*",
+                    c=list(range(0, n_iters)),
+                    alpha=alpha,
+                    cmap="plasma",
+                )
+            # Pop the first entry.
+            history.append(values)
+            history = history[-n_hist:]
+
+        plt.xticks([])
+        plt.yticks([])
+        plt.pause(pause_time)
+        plt.clf()
+
+        full_iters += 1
+        if positive_reset and (full_iters % n_reset == 0):
+            values = np.array([[0.0, 0.0]])
+            values = np.swapaxes(values, 0, 1)
+            a, b, c = random_abc()
+
+def _main() -> None:
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Run hog along visualiser", 
+        epilog=(
+            "`nreset=1 creates very chaotic (and cool) visuals. "
+            "Larger numbers (e.g. 10) creates more stable structures."
+        )
+    )
+
+    parser.add_argument(
+        "--niters",
+        nargs="?",
+        default=1_000,
+        type=int,
+        help="number of hop along iterations drawn per frame (default 1000)",
+    )
+    parser.add_argument(
+        "--nhist",
+        nargs="?",
+        default=10,
+        type=int,
+        help="number of frames that an iteration should remain on-screen (default 10)",
+    )
+    parser.add_argument(
+        "--nreset",
+        nargs="?",
+        default=10,
+        type=int,
+        help=(
+            "number of frames generated before parameters are randomised, "
+            "setting to zero keeps the same parameters throughout (default 10)"),
+    )
+    parser.add_argument(
+        "--fps",
+        nargs="?",
+        default=25.0,
+        type=float,
+        help="number of frames generated per second (default 25)",
+    )
+
+    parsed = parser.parse_args()
+
+    n_iters = parsed.niters
+    n_hist = parsed.nhist
+    n_reset = parsed.nreset
+    fps = parsed.fps
+    pause_time = 1.0 / fps
+
+    print(f"{n_iters=}\n{n_hist=}\n{n_reset=}\n{fps=}\n{pause_time=}")
+
+    plot_hop_along(
+        *random_abc(),
+        n_iters=n_iters,
+        n_hist=n_hist,
+        n_reset=n_reset,
+        pause_time=pause_time,
+    )
+
+
+if __name__ == "__main__":
+    _main()
